caf16e4078423260ae2cffe728c953e2457b892b
Ioannis Eleftheriou <me@yath.xyz>
2023-12-30 04:27:33 +0000

    Simplify flake: remove flake-utils

--- flake.nix
+++ flake.nix
@@ -2,10 +2,15 @@
   description = "Development environment for Space Station 14";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in { devShells.default = import ./shell.nix { inherit pkgs; }; });
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+        (system: function nixpkgs.legacyPackages.${system});
+    in {
+      devShells = forAllSystems
+        (pkgs: { default = import ./shell.nix { inherit pkgs; }; });
+    };
+
 }
